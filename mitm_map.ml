8a93d2cd3df93d71934391d91c9d9284692fda44
dakk <gessadavide@gmail.com>
2014-10-24 11:05:09 +0200

    hashtbl

--- mitm_map.ml
+++ mitm_map.ml
@@ -42,6 +42,8 @@ let pow x y =
 	in pow' x y
 ;;
 
+
+module Middle = Map.Make(String);;
 let bits = 20;;
 let hexchar = bits / 4;;
 let keynum = pow 2 bits;;
@@ -57,15 +59,14 @@ let int_to_hexs i =
 
 (* k : x = keynum : 100            x = 100 * k / keynum *)
 
-let middleMake d mx =
-	let rec middle k =
+let middleMake d =
+	let rec middle k m =
 		if k <= keynum then
 			let khex = (int_to_hexs k) in
 			if k mod 50000 = 0 then Printf.printf "%d ... (%d%%) %! " k (k*100/keynum) else ();
-			Hashtbl.add mx (Encrypt.encrypt khex d) khex;
-			middle (k+1)
-		else ()
-	in middle 0
+			middle (k+1) (Middle.add (Encrypt.encrypt khex d) khex m)
+		else m
+	in middle 0 Middle.empty
 ;;
 
 
@@ -75,23 +76,43 @@ let middleAttackSingle mx y =
 		let k2hex = (int_to_hexs k2) in
 		let ed = Decrypt.decrypt k2hex y in
 			try
-				let k1 = Hashtbl.find mx ed in Printf.printf "k1 = %s && k2 = %s\n%!" k1 k2hex
+				let k1 = Middle.find ed mx in Printf.printf "k1 = %s && k2 = %s\n%!" k1 k2hex
 			with Not_found -> ();
 	done
 ;;
 
 
+let middleJob mx y istart iend i =
+	Printf.printf "%d - Start from %d to %d\n%!" i istart iend;
+	for k2 = istart to iend do
+		let k2hex = (int_to_hexs k2) in
+		let ed = Decrypt.decrypt k2hex y in
+			try
+				let k1 = Middle.find ed mx in Printf.printf "%d - Found! k1 = %s && k2 = %s\n%!" i k1 k2hex
+			with Not_found -> ();
+	done;
+	Printf.printf "%i - End\n%!" i;
+;;
 
 
-let dataX = "7B652692CFB963A7";;
-let dataY = "A373589BC4F1A5D3";;
-let middleX = Hashtbl.create keynum;;
 
+(* Multi process attack *)
+(*let middleAttack mx y =
+	let step = keynum/procs in
+	let rec deploy nxst i =
+		if i = 0 then ()
+		else
+			match fork () with
+				| 0 -> middleJob mx y nxst (nxst+step) i
+				| pid -> deploy (nxst+step) (i-1)
+	in deploy 0 procs
+;;
+*)
 
 
 let () =
-	Printf.printf "Creating middleX...\n%!";
-	middleMake dataX middleX;
-	Printf.printf "\nSearching meets...\n%!"; 
-	middleAttackSingle middleX dataY
+	let dataX = "7B652692CFB963A7" in
+	let dataY = "A373589BC4F1A5D3" in
+	let middleX = Printf.printf "Creating middleX...\n%!"; middleMake Encrypt.encrypt dataX in
+	Printf.printf "\nSearching meets...\n%!"; middleAttackSingle middleX dataY
 ;;
